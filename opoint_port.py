89e17abe4a8b957fa8b86273161896395bc0e22a
kychen@davidson.edu <kyle@mutians-mbp.wireless.davidson.edu>
2018-03-29 13:22:18 -0400

    second commit

--- opoint_port.py
+++ opoint_port.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import ast
 import os
 import glob, os
 import gzip
@@ -20,22 +21,33 @@ def main():
     all_opoint = all_moment.loc[all_moment['feed_source'] == 'opoint']
     os.chdir("/Users/kyle/Documents/2018/CSC371MachineLearning/hw04/opoint/2018/03/01")
     i = 0
-    lst_str = []
-    for file in glob.glob("*.json.xml"):
-        with open(file) as f:
-            print file
-            try:
-                json_parsed = json.load(f)
-                if (json_parsed["searchresult"]["documents"] != 0):
-                    feed_article_id_str = str(json_parsed["searchresult"]["document"][0]["id_site"]) + "_" +  str(json_parsed["searchresult"]["document"][0]["id_article"])
-                    if((all_opoint['feed_article_id'] == feed_article_id_str).any()):
-                        lst_str.append(file+":"+feed_article_id_str)
-            except ValueError:
-                print 'Decoding JSON has failed'
+
+    # lst_str = []
+    # test = all_opoint.loc[all_opoint['feed_article_id'] == '550_3006626']
+    # print test
+    # for file in glob.glob("*.json.xml"):
+    #     with open(file) as f:
+    #         # print file
+    #         try:
+    #             json_parsed = json.load(f)
+    #             document_count = json_parsed["searchresult"]["documents"]
+    #             if (document_count != 0):
+    #                 for i in range(document_count):
+    #                     feed_article_id_str = str(json_parsed["searchresult"]["document"][i]["id_site"]) + "_" +  str(json_parsed["searchresult"]["document"][i]["id_article"])
+    #                     if((all_opoint['feed_article_id'] == feed_article_id_str).any()):
+    #                         print file+":"+feed_article_id_str
+    #                         lst_str.append(file+":"+feed_article_id_str)
+    #             else:
+    #                 print "0\n";
+    #         except ValueError:
+    #             print 'Decoding JSON has failed'
+    # print lst_str
+    # for item in lst_str:
+    #     with open("file_lookup.txt", "w") as text_file:
+    #         text_file.write(item)
+    #         text_file.write('\n')
 
 
-
-    print list_str
 
 
     # print all_opoint
